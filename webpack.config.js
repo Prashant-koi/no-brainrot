4ccf54d2a61af954b66459bdc5b88e68f03ad952
Prasant Koirala <koiralaprashanta10@gmail.com>
2025-12-03 14:04:44 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    background: './src/background/index.ts',
+    content: './src/content/index.ts',
+    popup: './src/popup/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '.' },
+        { from: 'src/popup/popup.html', to: 'popup.html' },
+      ],
+    }),
+  ],
+};
